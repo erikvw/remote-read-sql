b846ffac5c2bf859bdabd0b64b3dc0fc995eff52
erikvw <ew2789@gmail.com>
2025-10-22 19:49:49 -0500

    initial

---
+++ docs/source/conf.py
@@ -0,0 +1,57 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "remote-read-sql"
+copyright = "2025, Erik van Widenfelt"  # noqa: A001
+author = "Erik van Widenfelt"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_title = "Remote read_sql"
+html_static_path = ["_static"]
+
+html_sidebars = {
+    "**": [
+        "sidebar/scroll-start.html",
+        "sidebar/brand.html",
+        "sidebar/search.html",
+        "sidebar/navigation.html",
+        "sidebar/ethical-ads.html",
+        "sidebar/scroll-end.html",
+    ]
+}
+
+pygments_style = "sphinx"
+
+# EPUB options
+epub_show_urls = "footnote"
+
+
+html_context = {
+    "display_github": True,
+    "github_user": "erikvw",
+    "github_repo": "remote-read-sql",
+    "github_version": "main",
+}
