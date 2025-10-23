7a839c41f2dd472fb0b5e809f5e240e58ec732f8
erikvw <ew2789@gmail.com>
2025-10-23 06:55:59 -0500

    make main func a context manager, update readme

--- src/remote_read_sql/remote_read_sql.py
+++ src/remote_read_sql/remote_read_sql.py
@@ -1,3 +1,4 @@
+import contextlib
 import sys
 from pathlib import Path
 
@@ -13,19 +14,21 @@ class InvalidSqlQueryError(Exception):
     pass
 
 
+@contextlib.contextmanager
 def remote_read_sql(
-    sql_query: str,
+    sql_query: str | None = None,
     *,
     ssh_config_path: Path,
     my_cnf_path: Path,
     my_cnf_connection_name: str,
-    db_name: str | None = None,
+    db_name: str,
 ) -> pd.DataFrame:
     """Read sql query into dataframe via ssh tunnel and mysql connection."""
     df = None
-    parsed = parse_one(sql_query, read="mysql")
-    if not isinstance(parsed, exp.Select):
-        raise InvalidSqlQueryError("Only SELECT statements are allowed.")
+    if sql_query:
+        parsed = parse_one(sql_query, read="mysql")
+        if not isinstance(parsed, exp.Select):
+            raise InvalidSqlQueryError("Only SELECT statements are allowed.")
     with (
         get_ssh_connection(ssh_config_path) as local_bind_port,
         get_db_connection(
@@ -35,8 +38,11 @@ def remote_read_sql(
             db_name=db_name,
         ) as db_conn,
     ):
-        try:
-            df = pd.read_sql(sql_query, con=db_conn)
-        except OperationalError as e:
-            sys.stdout.write(f"Error executing query: {e}\n")
-    return df
+        if not sql_query:
+            yield db_conn
+        else:
+            try:
+                df = pd.read_sql(sql_query, con=db_conn)
+            except OperationalError as e:
+                sys.stdout.write(f"Error executing query: {e}\n")
+        return df
